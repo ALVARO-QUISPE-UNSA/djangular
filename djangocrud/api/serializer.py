f2fdcc32aacfd5c229f936578d71c51a54822dc5
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-07-12 21:40:58 -0500

    Se añadió las configuraciones y se creó el modelo serializado de Movie

---
+++ djangocrud/api/serializer.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import fields, serializers
+from .models import Movie
+
+class MovieSerializer(serializers.ModelField):
+    class Meta:
+        model = Movie
+        fields = ('id', 'title', 'desc', 'year')
